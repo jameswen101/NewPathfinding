6bf5e777daa541798cb45f358933ea335a47712b
jameswen101 <148604180+jameswen101@users.noreply.github.com>
2025-07-10 18:38:15 -0700

    SFX Added to Main Menu

--- 1333_Template/Assets/1333_RTS/StudentWork/Scripts/OtherScenes/IntroScreen.cs
+++ 1333_Template/Assets/1333_RTS/StudentWork/Scripts/OtherScenes/IntroScreen.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class IntroScreen : MonoBehaviour
 {
     public Button playButton;
-    public AudioSource GhostMusic;
+    [SerializeField] private IntroAudioManager introAudioManager;
 
     void Start()
     {
         playButton.onClick.AddListener(StartGame);
+        introAudioManager.PlayMusic("Gamela", true);
     }
 
     public void StartGame()
     {
+        introAudioManager.StopMusic(); // stop music
+        introAudioManager.PlayMusic("Beep Short");
         SceneManager.LoadScene("PathfindingTest");
-        GhostMusic.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        introAudioManager.PlaySFX("Answer Button");
     }
 }
